15c57a691c74ccfffb0011905a16ed59084aa813
ruantees <ruant142@gmail.com>
2023-10-08 11:18:20 +0300

    Added the Char counter feature

--- app/src/main/java/com/example/wordcounter/MainActivity.kt
+++ app/src/main/java/com/example/wordcounter/MainActivity.kt
@@ -2,10 +2,43 @@ package com.example.wordcounter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Spinner
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textInput = findViewById<EditText>(R.id.text_input)
+        val charSpinner = findViewById<Spinner>(R.id.char_spinner)
+        val countButton = findViewById<Button>(R.id.count_button)
+        val countResultView = findViewById<TextView>(R.id.count_result_view)
+
+        // Populate the Spinner object
+        // Create an ArrayAdapter using the string array and a default spinner layout.
+        ArrayAdapter.createFromResource(
+                this,
+                R.array.count_options,
+                android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears.
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner.
+            charSpinner.adapter = adapter
+        }
+
+        countButton.setOnClickListener {
+            val textInputString = textInput.text.toString()
+            val count = WordCounter.getCharCount(textInputString)
+            countResultView.text = count.toString()
+        }
     }
+
+
+
+
 }
---
+++ app/src/main/java/com/example/wordcounter/WordCounter.kt
@@ -0,0 +1,9 @@
+package com.example.wordcounter
+
+class WordCounter {
+    companion object {
+        fun getCharCount(textInputString: String): Any {
+            return textInputString.length
+        }
+    }
+}
